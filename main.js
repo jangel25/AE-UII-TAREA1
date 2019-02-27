c2d21f8a5cd23ecd3d69f342e0755161680e9368
Angel <angel251095@gmail.com>
2019-02-27 11:16:45 -0700

    Tarea 1 - Unidad 2

---
+++ main.js
@@ -0,0 +1,7 @@
+var sq = require("./square");
+
+var area = sq.ar(2);
+var perimetro = sq.per(2);
+
+console.log("El área es: "+area);
+console.log("El perímetro es: "+perimetro);
---
+++ square.js
@@ -0,0 +1,10 @@
+function area(width){
+    return width*width;
+}
+
+function perimetro(width){
+    return 4*width;
+}
+
+module.exports.ar = area;
+module.exports.per = perimetro;
